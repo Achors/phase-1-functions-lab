f3b6c3e2a904893c2561483a1236826285ce62dd
Adolf <adolfnyerere@gmail.com>
2023-09-21 03:08:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+   let scuberHq = 42;  
+    let distance = (scuberHq - pickupLocation);
+    return (Math.abs(distance));
+};
+function distanceFromHqInFeet(pickupLocation) {
+    let blocks = distanceFromHqInBlocks(pickupLocation);
+    return (blocks * 264);
+}
+function distanceTravelledInFeet(start, destination){
+    
+    return ((Math.abs(destination - start)) * 264);
+}
+function calculatesFarePrice(start, destination) {
+    let fareDistance = distanceTravelledInFeet(start, destination);
+    if (fareDistance <= 400) {
+        return (0);
+    } else if (fareDistance > 400 && fareDistance < 2000) {
+        return ((fareDistance - 400) * 0.02);
+
+    } else if (fareDistance > 2000 && fareDistance < 2500) {
+        return (25);
+    } else {
+        return (`cannot travel that far`);
+    }
+}
